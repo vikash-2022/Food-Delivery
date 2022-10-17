d19902b11d1354fdb5249bce10cbda2ffdb13e2c
Vikash <vikash.danu.1.vd@gmail.com>
2022-10-17 23:53:53 +0530

    delet

--- header.php
+++ header.php
@@ -1,14 +1,10 @@
 <?php include"database.php"
-
 ?>
 <?php 
 session_start();
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
